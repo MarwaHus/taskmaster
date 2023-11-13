aba3504179b7a596466bf0f51cb7a6a7c891895d
MarwaHus <marwa.hu18@gmail.com>
2023-11-13 23:59:54 +0300

    lab33

--- app/src/main/java/com/amplifyframework/datastore/generated/model/Task.java
+++ app/src/main/java/com/amplifyframework/datastore/generated/model/Task.java
@@ -1,8 +1,10 @@
 package com.amplifyframework.datastore.generated.model;
 
 import com.amplifyframework.core.model.temporal.Temporal;
+import com.amplifyframework.core.model.annotations.BelongsTo;
 import com.amplifyframework.core.model.ModelIdentifier;
 
+import java.util.List;
 import java.util.UUID;
 import java.util.Objects;
 
@@ -12,40 +14,40 @@ import com.amplifyframework.core.model.AuthStrategy;
 import com.amplifyframework.core.model.Model;
 import com.amplifyframework.core.model.ModelOperation;
 import com.amplifyframework.core.model.annotations.AuthRule;
+import com.amplifyframework.core.model.annotations.Index;
 import com.amplifyframework.core.model.annotations.ModelConfig;
 import com.amplifyframework.core.model.annotations.ModelField;
 import com.amplifyframework.core.model.query.predicate.QueryField;
 
 import static com.amplifyframework.core.model.query.predicate.QueryField.field;
 
-/** This is an auto generated class representing the Product type in your schema. */
+/** This is an auto generated class representing the Task type in your schema. */
 @SuppressWarnings("all")
-@ModelConfig(pluralName = "Products", type = Model.Type.USER, version = 1, authRules = {
+@ModelConfig(pluralName = "Tasks", type = Model.Type.USER, version = 1, authRules = {
   @AuthRule(allow = AuthStrategy.PUBLIC, operations = { ModelOperation.CREATE, ModelOperation.UPDATE, ModelOperation.DELETE, ModelOperation.READ })
 })
+@Index(name = "byTeam", fields = {"teamId","name"})
 public final class Task implements Model {
   public static final QueryField ID = field("Task", "id");
-  public static final QueryField Title = field("Task", "title");
-  public static final QueryField Body = field("Task", "body");
+  public static final QueryField NAME = field("Task", "name");
+  public static final QueryField DESCRIPTION = field("Task", "description");
   public static final QueryField DATE_CREATED = field("Task", "dateCreated");
-  public static final QueryField State = field("Task", "state");
+  public static final QueryField PRODUCT_CATEGORY = field("Task", "productCategory");
+  public static final QueryField TEAM = field("Task", "teamId");
   private final @ModelField(targetType="ID", isRequired = true) String id;
   private final @ModelField(targetType="String", isRequired = true) String name;
   private final @ModelField(targetType="String") String description;
   private final @ModelField(targetType="AWSDateTime") Temporal.DateTime dateCreated;
   private final @ModelField(targetType="productCategoryEnum") ProductCategoryEnum productCategory;
+  private final @ModelField(targetType="Team") @BelongsTo(targetName = "teamId", targetNames = {"teamId"}, type = Team.class) Team team;
   private @ModelField(targetType="AWSDateTime", isReadOnly = true) Temporal.DateTime createdAt;
   private @ModelField(targetType="AWSDateTime", isReadOnly = true) Temporal.DateTime updatedAt;
-    private String title;
-    private String body;
-    private NameStep state;
-
-    /** @deprecated This API is internal to Amplify and should not be used. */
+  /** @deprecated This API is internal to Amplify and should not be used. */
   @Deprecated
    public String resolveIdentifier() {
     return id;
   }
-
+  
   public String getId() {
       return id;
   }
@@ -61,9 +63,13 @@ public final class Task implements Model {
   public Temporal.DateTime getDateCreated() {
       return dateCreated;
   }
-
+  
   public ProductCategoryEnum getProductCategory() {
       return productCategory;
+  }
+  
+  public Team getTeam() {
+      return team;
   }
   
   public Temporal.DateTime getCreatedAt() {
@@ -74,12 +80,13 @@ public final class Task implements Model {
       return updatedAt;
   }
   
-  private Task(String id, String name, String description, Temporal.DateTime dateCreated, ProductCategoryEnum productCategory) {
+  private Task(String id, String name, String description, Temporal.DateTime dateCreated, ProductCategoryEnum productCategory, Team team) {
     this.id = id;
     this.name = name;
     this.description = description;
     this.dateCreated = dateCreated;
     this.productCategory = productCategory;
+    this.team = team;
   }
   
   @Override
@@ -89,14 +96,15 @@ public final class Task implements Model {
       } else if(obj == null || getClass() != obj.getClass()) {
         return false;
       } else {
-      Task product = (Task) obj;
-      return ObjectsCompat.equals(getId(), product.getId()) &&
-              ObjectsCompat.equals(getName(), product.getName()) &&
-              ObjectsCompat.equals(getDescription(), product.getDescription()) &&
-              ObjectsCompat.equals(getDateCreated(), product.getDateCreated()) &&
-              ObjectsCompat.equals(getProductCategory(), product.getProductCategory()) &&
-              ObjectsCompat.equals(getCreatedAt(), product.getCreatedAt()) &&
-              ObjectsCompat.equals(getUpdatedAt(), product.getUpdatedAt());
+      Task task = (Task) obj;
+      return ObjectsCompat.equals(getId(), task.getId()) &&
+              ObjectsCompat.equals(getName(), task.getName()) &&
+              ObjectsCompat.equals(getDescription(), task.getDescription()) &&
+              ObjectsCompat.equals(getDateCreated(), task.getDateCreated()) &&
+              ObjectsCompat.equals(getProductCategory(), task.getProductCategory()) &&
+              ObjectsCompat.equals(getTeam(), task.getTeam()) &&
+              ObjectsCompat.equals(getCreatedAt(), task.getCreatedAt()) &&
+              ObjectsCompat.equals(getUpdatedAt(), task.getUpdatedAt());
       }
   }
   
@@ -108,6 +116,7 @@ public final class Task implements Model {
       .append(getDescription())
       .append(getDateCreated())
       .append(getProductCategory())
+      .append(getTeam())
       .append(getCreatedAt())
       .append(getUpdatedAt())
       .toString()
@@ -117,12 +126,13 @@ public final class Task implements Model {
   @Override
    public String toString() {
     return new StringBuilder()
-      .append("Product {")
+      .append("Task {")
       .append("id=" + String.valueOf(getId()) + ", ")
       .append("name=" + String.valueOf(getName()) + ", ")
       .append("description=" + String.valueOf(getDescription()) + ", ")
       .append("dateCreated=" + String.valueOf(getDateCreated()) + ", ")
       .append("productCategory=" + String.valueOf(getProductCategory()) + ", ")
+      .append("team=" + String.valueOf(getTeam()) + ", ")
       .append("createdAt=" + String.valueOf(getCreatedAt()) + ", ")
       .append("updatedAt=" + String.valueOf(getUpdatedAt()))
       .append("}")
@@ -147,6 +157,7 @@ public final class Task implements Model {
       null,
       null,
       null,
+      null,
       null
     );
   }
@@ -156,38 +167,12 @@ public final class Task implements Model {
       name,
       description,
       dateCreated,
-      productCategory);
+      productCategory,
+      team);
   }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
-
-    public NameStep getState() {
-        return state;
-    }
-
-    public void setState(NameStep state) {
-        this.state = state;
-    }
-
-    public interface NameStep {
+  public interface NameStep {
     BuildStep name(String name);
-
-        String name();
-    }
+  }
   
 
   public interface BuildStep {
@@ -196,6 +181,7 @@ public final class Task implements Model {
     BuildStep description(String description);
     BuildStep dateCreated(Temporal.DateTime dateCreated);
     BuildStep productCategory(ProductCategoryEnum productCategory);
+    BuildStep team(Team team);
   }
   
 
@@ -205,16 +191,18 @@ public final class Task implements Model {
     private String description;
     private Temporal.DateTime dateCreated;
     private ProductCategoryEnum productCategory;
+    private Team team;
     public Builder() {
       
     }
     
-    private Builder(String id, String name, String description, Temporal.DateTime dateCreated, ProductCategoryEnum productCategory) {
+    private Builder(String id, String name, String description, Temporal.DateTime dateCreated, ProductCategoryEnum productCategory, Team team) {
       this.id = id;
       this.name = name;
       this.description = description;
       this.dateCreated = dateCreated;
       this.productCategory = productCategory;
+      this.team = team;
     }
     
     @Override
@@ -226,7 +214,8 @@ public final class Task implements Model {
           name,
           description,
           dateCreated,
-          productCategory);
+          productCategory,
+          team);
     }
     
     @Override
@@ -235,13 +224,8 @@ public final class Task implements Model {
         this.name = name;
         return this;
     }
-
-      @Override
-      public String name() {
-          return null;
-      }
-
-      @Override
+    
+    @Override
      public BuildStep description(String description) {
         this.description = description;
         return this;
@@ -259,6 +243,12 @@ public final class Task implements Model {
         return this;
     }
     
+    @Override
+     public BuildStep team(Team team) {
+        this.team = team;
+        return this;
+    }
+    
     /**
      * @param id id
      * @return Current Builder instance, for fluent method chaining
@@ -271,8 +261,8 @@ public final class Task implements Model {
   
 
   public final class CopyOfBuilder extends Builder {
-    private CopyOfBuilder(String id, String name, String description, Temporal.DateTime dateCreated, ProductCategoryEnum productCategory) {
-      super(id, name, description, dateCreated, productCategory);
+    private CopyOfBuilder(String id, String name, String description, Temporal.DateTime dateCreated, ProductCategoryEnum productCategory, Team team) {
+      super(id, name, description, dateCreated, productCategory, team);
       Objects.requireNonNull(name);
     }
     
@@ -295,12 +285,17 @@ public final class Task implements Model {
      public CopyOfBuilder productCategory(ProductCategoryEnum productCategory) {
       return (CopyOfBuilder) super.productCategory(productCategory);
     }
+    
+    @Override
+     public CopyOfBuilder team(Team team) {
+      return (CopyOfBuilder) super.team(team);
+    }
   }
   
 
-  public static class ProductIdentifier extends ModelIdentifier<Task> {
+  public static class TaskIdentifier extends ModelIdentifier<Task> {
     private static final long serialVersionUID = 1L;
-    public ProductIdentifier(String id) {
+    public TaskIdentifier(String id) {
       super(id);
     }
   }
