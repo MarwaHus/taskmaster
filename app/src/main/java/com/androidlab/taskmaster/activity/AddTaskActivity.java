967e7161795d7ded07da2497a74a20df47ec6777
MarwaHus <marwa.hu18@gmail.com>
2023-11-15 13:06:58 +0300

    lab33update

--- app/src/main/java/com/androidlab/taskmaster/activity/AddTaskActivity.java
+++ app/src/main/java/com/androidlab/taskmaster/activity/AddTaskActivity.java
@@ -13,68 +13,41 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.amplifyframework.api.graphql.model.ModelMutation;
+import com.amplifyframework.api.graphql.model.ModelQuery;
 import com.amplifyframework.core.Amplify;
 import com.amplifyframework.core.model.temporal.Temporal;
 import com.amplifyframework.datastore.generated.model.ProductCategoryEnum;
 import com.amplifyframework.datastore.generated.model.Task;
+import com.amplifyframework.datastore.generated.model.Team;
 import com.androidlab.taskmaster.R;
 import com.androidlab.taskmaster.TaskEnum;
+import com.google.android.material.snackbar.Snackbar;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class AddTaskActivity extends AppCompatActivity {
     private int totalTasks = 0;
     private TextView totalTasksTextView;
     public static final String TAG = "AddProductActivity";
+    Spinner teamSpinner = null;
+    Spinner taskSpinner = null;
+
+    CompletableFuture<List<Team>> teamsFuture = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
-
-
-       // totalTasks = taskDataBase.taskDao().findAll().size();
+        // totalTasks = taskDataBase.taskDao().findAll().size();
         totalTasksTextView = findViewById(R.id.textView5);
         totalTasksTextView.setText("Total Tasks: " + totalTasks);
-
-        Button submitButton = findViewById(R.id.submit_button);
-        submitButton.setOnClickListener(v -> {
-//            Task newTask = new Task(
-//                    ((EditText) findViewById(R.id.task_title)).getText().toString(),
-//                    ((EditText) findViewById(R.id.task_body)).getText().toString(),
-//                    TaskEnum.fromString(((Spinner) findViewById(R.id.spinner)).getSelectedItem().toString()),
-//                    new Date());
-
-          //  taskDataBase.taskDao().insertTask(newTask);
-
-            String name = ((EditText)findViewById(R.id.task_title)).getText().toString();
-            String description = ((EditText)findViewById(R.id.task_body)).getText().toString();
-            Spinner taskSpinner = findViewById(R.id.spinner);
-            taskSpinner.setAdapter(new ArrayAdapter<>(
-                    this,
-                    android.R.layout.simple_spinner_item,
-                    TaskEnum.values()));
-
-            Task newProduct = Task.builder()
-                    .name(name)
-                    .description(description)
-                    .dateCreated(new Temporal.DateTime(new Date(), 0))
-                    .productCategory((ProductCategoryEnum) taskSpinner.getSelectedItem()).build();
-
-            Amplify.API.mutate(
-                    ModelMutation.create(newProduct),
-                    successResponse -> {
-                        Log.i(TAG, "AddTaskActivity.onCreate(): made a task successfully");
-                        totalTasks++;
-                        runOnUiThread(() -> totalTasksTextView.setText("Total Tasks: " + totalTasks));
-                        Toast.makeText(this, "Task Added Successfully", Toast.LENGTH_SHORT).show();
-                    },
-                    failureResponse -> {
-                        Log.e(TAG, "AddTaskActivity.onCreate(): failed with this response" + failureResponse);
-                        Toast.makeText(this, "Failed to add task. Please try again.", Toast.LENGTH_SHORT).show();
-                    }
-            );
-        });
-
+        teamsFuture = new CompletableFuture<List<Team>>();
+        setUpSpinner();
+        setUpSaveButton();
 
         Button backButton = findViewById(R.id.backk_button);
         backButton.setOnClickListener(v -> {
@@ -82,4 +55,57 @@ public class AddTaskActivity extends AppCompatActivity {
             startActivity(intent);
         });
     }
-}
+    public void setUpSpinner() {
+
+
+        taskSpinner = (Spinner) findViewById(R.id.spinner);
+        taskSpinner.setAdapter(new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_item,
+                ProductCategoryEnum.values()
+        ));
+    }
+ private void setUpSaveButton() {
+     Button submitButton = findViewById(R.id.submit_button);
+     submitButton.setOnClickListener(v -> {
+         String name = ((EditText) findViewById(R.id.task_title)).getText().toString();
+         String description = ((EditText) findViewById(R.id.task_body)).getText().toString();
+         String selectedTeamString = teamSpinner.getSelectedItem().toString();
+         List<Team> teams = null;
+         try {
+             teams = teamsFuture.get();
+         } catch (InterruptedException ie) {
+             Log.e(TAG, " InterruptedException while getting contacts");
+         } catch (ExecutionException ee) {
+             Log.e(TAG, " ExecutionException while getting contacts");
+         }
+
+         Team selectedTeam = teams.stream().filter(c -> c.getTeamName().equals(selectedTeamString)).findAny().orElseThrow(RuntimeException::new);
+
+         Task newProduct = Task.builder()
+                 .name(name)
+                 .description(description)
+                 .dateCreated(new Temporal.DateTime(new Date(), 0))
+                 .productCategory((ProductCategoryEnum) taskSpinner.getSelectedItem())
+                 .team(selectedTeam)
+                 .build();
+
+         Amplify.API.mutate(
+                 ModelMutation.create(newProduct),
+                 successResponse -> {
+                     Log.i(TAG, "AddTaskActivity.onCreate(): made a task successfully");
+                     totalTasks++;
+                     runOnUiThread(() -> totalTasksTextView.setText("Total Tasks: " + totalTasks));
+                     Toast.makeText(this, "Task Added Successfully", Toast.LENGTH_SHORT).show();
+                 },
+                 failureResponse -> {
+                     Log.e(TAG, "AddTaskActivity.onCreate(): failed with this response" + failureResponse);
+                     Toast.makeText(this, "Failed to add task. Please try again.", Toast.LENGTH_SHORT).show();
+                 }
+         );
+         Snackbar.make(findViewById(R.id.addTaskActivity), "Task saved!", Snackbar.LENGTH_SHORT).show();
+
+     });
+ }
+
+    }
--- app/src/main/java/com/androidlab/taskmaster/activity/MainActivity.java
+++ app/src/main/java/com/androidlab/taskmaster/activity/MainActivity.java
@@ -15,9 +15,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.amplifyframework.api.graphql.model.ModelMutation;
 import com.amplifyframework.api.graphql.model.ModelQuery;
 import com.amplifyframework.core.Amplify;
+import com.amplifyframework.datastore.generated.model.ProductCategoryEnum;
 import com.amplifyframework.datastore.generated.model.Task;
+import com.amplifyframework.datastore.generated.model.Team;
 import com.androidlab.taskmaster.R;
 import com.androidlab.taskmaster.adapter.TaskAdapter;
 
@@ -29,20 +32,19 @@ public class MainActivity extends AppCompatActivity {
     public static final String TASK_BODY_TAG = "body";
     public static final String TASK_STATE_TAG = "state";
     public static final String DATABASE_NAME = "add_new_task";
+    public static final String TASK_TEAM_TAG = "team" ;
     List<Task> tasks = new ArrayList<com.amplifyframework.datastore.generated.model.Task>();
     TaskAdapter adapter;
-
+    SharedPreferences preferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        init();
         setUpListRecyclerView();
         Button addTaskButton = (Button) findViewById(R.id.addButton);
         Button allTasksButton = (Button) findViewById(R.id.allButton);
         Button settingsButton = (Button) findViewById(R.id.settingButton);
-
-        //tasks = taskDataBase.taskDao().findAll();
         setUpListRecyclerView();
         addTaskButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,11 +76,52 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView ListRecyclerView = findViewById(R.id.recyclerView);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         ListRecyclerView.setLayoutManager(layoutManager);
-        //tasks.add(new Task("TASK 1", "STUDY",TaskEnum.complete,new Date()));
-       // adapter = new TaskAdapter(tasks, this);
-        adapter = new TaskAdapter(tasks, this);
-        // Set the adapter after initializing adapter
 
+//
+//        Team team1 =
+//                Team.builder()
+//                    .teamName("Team1")
+//                    .build();
+//        Team team2 =
+//                Team.builder()
+//                        .teamName("Team2")
+//                        .build();
+//        Team team3 =
+//                Team.builder()
+//                        .teamName("Team3")
+//                        .build();
+//        Amplify.API.mutate(
+//               ModelMutation.create(team1),
+//               successResponse -> Log.i(TAG, "MainActivity.onCreate(): made a team successfully"),
+//               failureResponse -> Log.i(TAG, "MainActivity.onCreate(): team failed with this response: "+failureResponse)
+//       );
+//        Amplify.API.mutate(
+//                ModelMutation.create(team2),
+//                successResponse -> Log.i(TAG, "MainActivity.onCreate(): made a team successfully"),
+//                failureResponse -> Log.i(TAG, "MainActivity.onCreate(): team failed with this response: "+failureResponse)
+//        );
+//        Amplify.API.mutate(
+//                ModelMutation.create(team3),
+//                successResponse -> Log.i(TAG, "MainActivity.onCreate(): made a team successfully"),
+//                failureResponse -> Log.i(TAG, "MainActivity.onCreate(): team failed with this response: "+failureResponse)
+//        );
+        Amplify.API.query(
+                ModelQuery.list(com.amplifyframework.datastore.generated.model.Task.class),
+                success ->
+                {
+                    Log.i(TAG, "Read Product successfully");
+                    tasks.clear();
+                    for (com.amplifyframework.datastore.generated.model.Task databaseProduct : success.getData()){
+                        tasks.add(databaseProduct);
+                    }
+                    runOnUiThread(() ->{
+                        adapter.notifyDataSetChanged();
+                    });
+                },
+                failure -> Log.i(TAG, "Did not read products successfully")
+        );
+
+        adapter = new TaskAdapter(tasks, this);
         ListRecyclerView.setAdapter(adapter);
 
     }
@@ -89,26 +132,10 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         String username = preferences.getString(SettingsActivity.USER_NAME, "NO USERNAME");
         ((TextView) findViewById(R.id.nametextView)).setText(getString(R.string.username_main, username));
-       // tasks = taskDataBase.taskDao().findAll();
-
-        Amplify.API.query(
-                ModelQuery.list(com.amplifyframework.datastore.generated.model.Task.class),
-                success ->
-                {
-                    Log.i(TAG, "Read Product successfully");
-                    tasks.clear();
-                    for (com.amplifyframework.datastore.generated.model.Task databaseProduct : success.getData()){
-                  tasks.add(databaseProduct);
-                    }
-                    runOnUiThread(() ->{
-                        adapter.notifyDataSetChanged();
-                    });
-                },
-                failure -> Log.i(TAG, "Did not read products successfully")
-        );
-
-
         setUpListRecyclerView();
     }
-
+    private void init() {
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        tasks = new ArrayList<>();
+    }
 }
